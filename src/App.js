12718f14bfffa4cdc407de46c9cbc2b41c95add0
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-05-17 14:18:45 +0700

    finishTabLogo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronDown, faListCheck } from "@fortawesome/free-solid-svg-icons"
+import { faChevronDown,faChevronUp, faListCheck, faQuestion } from "@fortawesome/free-solid-svg-icons"
 import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenQB, setIsOpenQB] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
   
-  
-  
+  //Open child of PersonnelEvoluation
   const toggleOpen = () => {
     setIsOpen(!isOpen);
+  };
+
+  //Open child of QuestionBank
+  const toggleOpenQuestionBank = () => {
+    setIsOpenQB(!isOpenQB);
   };
 
 
@@ -24,28 +29,23 @@ function App() {
         <FontAwesomeIcon className="iconCheDown" icon={faChevronDown} style={{height: "25%"}}/>
         </div>
         <div className='TagMenu'>
-          <div className={`PersonelEvaluation ${isOpen ? 'green' : ''}`} onClick={toggleOpen}>
-            <div className='ChildPersonelEvaluation'>
+          <div className={`PersonelEvaluation ${isOpen ? 'green' : ''}`}>
+            <div className='ChildPersonelEvaluation'  onClick={toggleOpen}>
             <FontAwesomeIcon className="iconCheckList" icon={faListCheck} style={{height: "35%"}}/>
             <h3 className="h3PersonelEvaluation"> ĐÁNH GIÁ NHÂN SỰ</h3>
-            <FontAwesomeIcon className="iconCheDownPer" icon={faChevronDown} style={{height: "25%"}}/>
+            {isOpen ? (
+              <FontAwesomeIcon className="iconChePer" icon={faChevronUp} style={{ height: '25%' }} />
+            ) : (
+              <FontAwesomeIcon className="iconChePer" icon={faChevronDown} style={{ height: '25%' }} />
+            )}
             </div>
             <div className='ChildMenu'>
             {isOpen && (
             <>
-            <div className="PersonelEvaluation">
-              <div className="ChildPersonelEvaluation">
-                <FontAwesomeIcon className="iconCheckList" icon={faListCheck} style={{height: "35%"}}/>
-                <h3 className="h3PersonelEvaluation">ĐÁNH GIÁ NHÂN SỰ</h3>
-                <FontAwesomeIcon className="iconCheDownPer" icon={faChevronDown} style={{height: "25%"}}/>
-              </div>
-            </div>
-
-            <div className="QuestionBanking2">
-              <div className="ChildPersonelEvaluation">
-                <FontAwesomeIcon className="iconCheckList" icon={faListCheck} style={{height: "35%"}}/>
-                <h3 className="h3PersonelEvaluation">ĐÁNH GIÁ NHÂN SỰ</h3>
-                <FontAwesomeIcon className="iconCheDownPer" icon={faChevronDown} style={{height: "25%"}}/>
+            <div className={`QuestionBank ${isOpenQB ? 'green' : ''}`}>
+              <div className="childQuestionBank" onClick={toggleOpenQuestionBank}>
+                <FontAwesomeIcon className="iconQuestion" icon={faQuestion} style={{height: "35%"}}/>
+                <h3 className="h3QuestionBank">Ngân hàng câu hỏi</h3>
               </div>
             </div>
             </>
